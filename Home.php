e38312d8b5be51b3a5c6e07f7227dafc81d7b91f
Ahmed Kamel <ahmedkamel@acm.org>
2014-12-22 17:33:18 +0200

    Revert "All - First"
    This reverts commit 02d0ffcdf3cb445827356f904bb9a34ffd7f6ac5.

--- Home.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$content_title = "Welcome to Home!";
-$content = "sdfsdfsfsdfsdfsdfsfsdfsdfsdfsfsd<br/>fsdfsdfsfsdfsdfsdfsfsdfsdfsdfsfsdf<br/>sdfsdfsfsdfsdfsdfsfsdfsdfsdfsfsdfs<br/>dfsdfsfsdfsdfsdfsfsdfsdfsdfsfsdfsd<br/>fsdfsfsdfsdfsdfsfsdfsdfsdfsfsdfs<br/>dfsdfsfsdfsdfsdfsfsdfsdfsdfsfsd<br/>fsdfsdfsfsdfsdfsdfsfsdfsdfsdfsfsdf<br/>sdfsdfsfsdfsdfsdfsfsdfsdfsdfsfs<br/>dfsdfsdfsfsdfsdfsdfsfsdfsdfsdfs<br/>fsdfsdfsdfsfsdfsdfsdfsfsdfsdfsdf<br/>sfsdfsdfsdfsfsdfsdfsdfsfsdfsd<br/>fsdfsfsdfsdfsdfsfsdfsdfs<br/>dfsfsdfsdfsdfsfsdf";
-
-include 'Template/Template+.php';
-
--- Models/Credentials.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$host = "localhost";
-$username = "root";
-$password = "";
-$database = "db";
-
-?>
-
