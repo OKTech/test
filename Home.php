02d0ffcdf3cb445827356f904bb9a34ffd7f6ac5
Ahmed Kamel <ahmedkamel@acm.org>
2014-12-22 17:27:09 +0200

    All - First
    All - First

---
+++ Home.php
@@ -0,0 +1,7 @@
+<?php
+
+$content_title = "Welcome to Home!";
+$content = "sdfsdfsfsdfsdfsdfsfsdfsdfsdfsfsd<br/>fsdfsdfsfsdfsdfsdfsfsdfsdfsdfsfsdf<br/>sdfsdfsfsdfsdfsdfsfsdfsdfsdfsfsdfs<br/>dfsdfsfsdfsdfsdfsfsdfsdfsdfsfsdfsd<br/>fsdfsfsdfsdfsdfsfsdfsdfsdfsfsdfs<br/>dfsdfsfsdfsdfsdfsfsdfsdfsdfsfsd<br/>fsdfsdfsfsdfsdfsdfsfsdfsdfsdfsfsdf<br/>sdfsdfsfsdfsdfsdfsfsdfsdfsdfsfs<br/>dfsdfsdfsfsdfsdfsdfsfsdfsdfsdfs<br/>fsdfsdfsdfsfsdfsdfsdfsfsdfsdfsdf<br/>sfsdfsdfsdfsfsdfsdfsdfsfsdfsd<br/>fsdfsfsdfsdfsdfsfsdfsdfs<br/>dfsfsdfsdfsdfsfsdf";
+
+include 'Template/Template+.php';
+
