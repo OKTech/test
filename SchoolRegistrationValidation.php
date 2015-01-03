832fdafbf413066c835ce3a24bbd405f3567a966
Omar Ali <en.omarali@gmail.com>
2015-01-03 21:00:28 +0200

    School Registration work

--- SchoolRegistrationValidation.php
+++ SchoolRegistrationValidation.php
@@ -11,9 +11,22 @@
 	$address = $_POST["addressTXT"];
 	$country = $_POST["country"];
         $grade = $_POST["grade"];
+        $photo = $_POST["upload"];
         
-        validateUsername($username);
-        validatePassword($password);
+        
+        function vlidateAll(){
+            validateUsername($username);
+            validatePassword($password);
+            validateCountry($country);
+            validateGrade($grade);
+            validateSchoolName($schoolName);
+            validateSchoolName($twitter);
+            validateSchoolName($facebook);
+            validateSchoolName($google);
+            validatePhone($phone);
+            validatePhone($fax);
+            validatePhoto();
+        }
         
         function validateUsername ($username){
             $len = strlen($username);
@@ -30,6 +43,46 @@
         function validatePassword ($password){
             $len = strlen($password);
             if ($len == 0) return false;
+            return true;
+        }
+        
+        function validateCountry ($country){
+            if ($country == 'Country') return false;
+            return true;
+        }
+        
+        function validateGrade ($grade){
+            if ($grade == 'Grade') return false;
+            return true;
+        }
+        
+        function validateSchoolName ($schoolName){
+            $len = strlen ($schoolName);
+            for ($i = 0; $i<$len; $i++){
+                if (!ctype_alpha ( $schoolName[$i] ) && $schoolName[$i] != ' '){
+                    return false;
+                }
+            }
+            return true;
+        }
+        
+        function isNum ($digit){
+            if ($digit >= '0' && $digit <= '9') return true;
+            return false;
+        }
+        
+        function validatePhone ($phone){
+            $len = strlen($phone);
+            if ($len < 8) return false;
+            if (!isNum($phone[0]) && $phone[0] != '+') return false;
+            for ($i=1; $i<$len; $i++){
+                if (!isNum($phone[$i])) return false;
+            }
+            return true;
+        }
+        
+        function validatePhoto (){
+            if (!isset($_POST[$photo])) return false;
             return true;
         }
     
