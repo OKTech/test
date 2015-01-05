e9821e0ee7f04db206743e0dba066b4953b7fd14
haytham-breaka <haytham.breaka@gmail.com>
2015-01-05 03:07:05 +0200

    Exam Page

--- Exam.php
+++ Exam.php
@@ -1,13 +1,27 @@
 <?php
 
 $header = "";
-$title = "Home";
-$content_title = "Welcome to Home!";
-$content = "
+$title = "Exam";
+$content_title = "";
+$content = '
+    <div class="ExamPaper" >
+        <div>
+            <h1 class="capitalize"> Web 12th Exam </h3>
+            <div style="text-align: left;">
+                    <h4> 1. Given 8 space-separated integers, find the sum of those in even places and the sum of those in odd places. Note: Even place means the 2nd, 4th, 6th or 8th numbers, while odd places are the 1st, 3rd, 5th and 7th numbers.</p></td> 
+            </div>
+        </div>
+        <textarea placeholder="Write your answer here!" class="textarea" type="textarea" name="answer" id="answer" style="width:90%;height:100px;"/>
+        </textarea>
+    </div>
+
+    <link rel="stylesheet" href="css/exam.css" type="text/css"/>
+   
+
     <script>
-        var LINK = 'homeLI';
+        var LINK = "homeLI";
     </script>
-";
+';
 
 
 include 'Template/MainTemplate.html';
